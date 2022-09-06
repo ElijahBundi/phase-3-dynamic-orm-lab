ca9b989c2a905a4ee87381217a34690e9e11b236
Elijah <bundielly@gmail.com>
2022-09-06 13:35:04 +0300

    commit tree fc3c5e1f4feff0436af5ac4843e9e37135e48312

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,27 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+
+    def self.column_names
+        column_names = []
+        table_info = DB[:conn].execute("PRAGMA table_info(#{self.table_name})")
+        
+        table_info.map do |column|
+            column_names << column["name"]
+        end
+        column_names.compact
+    end
+
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
+
+    def initialize(options={})
+        options.each do |key, value|
+            self.send("#{key}=", value)
+        end
+    end
 end
